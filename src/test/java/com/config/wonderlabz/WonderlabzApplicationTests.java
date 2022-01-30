b728c05633a49d9716e436a5387d55298df9ea91
David Mbaimbai <davymbaimbai@gmail.com>
2022-01-30 18:09:27 +0200

    Added config app implimentation

---
+++ src/test/java/com/config/wonderlabz/WonderlabzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.config.wonderlabz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderlabzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
